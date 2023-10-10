18f9f7703fe831b899f695db6f4c48e2b53e6e69
MokwaTheDeveloper <moffatmokwa12@gmail.com>
2023-10-10 22:42:45 +0300

    completed Assigment

---
+++ .idea/index.js
@@ -0,0 +1,22 @@
+// 1. Create variables for firstname, lastname, and email
+const firstname = 'YourFirstName';
+const lastname = 'YourLastName';
+const email = 'your.email@example.com';
+
+// 2. Create a variable for age
+const age = 25; // Replace with your actual age
+
+// 3. Create a variable for learning JavaScript with a boolean value
+const iAmLearningJavaScript = true;
+
+// 4. Create a sentence by concatenating the variables
+const sentence = `My name is ${firstname} ${lastname}, I am ${age} years old and it's ${iAmLearningJavaScript ? 'true' : 'false'} I am learning JavaScript.`;
+
+// Print out the values
+console.log(firstname);
+console.log(lastname);
+console.log(email);
+console.log(age);
+console.log(iAmLearningJavaScript);
+
+console.log(sentence);
